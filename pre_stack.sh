eddc23d56a4b6c48d18a4cee3fb60cb571b0370c
Ubuntu <ubuntu@devstack-master.alaskalocal>
2019-10-09 16:44:49 +0000

    Install git-review

--- pre_stack.sh
+++ pre_stack.sh
@@ -2,6 +2,7 @@
 sudo add-apt-repository ppa:longsleep/golang-backports -y
 sudo apt-get update
 sudo apt-get install golang-go -y
+sudo apt-get install git-review -y
 
 # Install sonobuoy
 go get -u -v github.com/heptio/sonobuoy
