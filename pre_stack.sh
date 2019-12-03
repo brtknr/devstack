64b96101120cfc4fba72d38c2987ab0c48e4c582
Bharat Kunwar <brtknr@bath.edu>
2019-12-03 11:27:04 +0000

    Use python3 by default in ubuntu

--- pre_stack.sh
+++ pre_stack.sh
@@ -4,5 +4,8 @@ sudo apt-get update
 sudo apt-get install golang-go -y
 sudo apt-get install git-review -y
 
+# Use python3 by default
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 10
+
 # ssh-keygen
 ssh-keygen
